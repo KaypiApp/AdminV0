c5cc4158092609a4044700fe0d25b541b7a52035
Laura Nuñez <njm0031533@est.univalle.edu>
2024-04-04 11:16:40 -0400

    Error en null solucionado

--- src/models/Linea.js
+++ src/models/Linea.js
@@ -7,8 +7,12 @@ const lineaTransporteSchema = new Schema({
     Pasajes:Array,
     Horarios:Array,
     Calles:Array,
-    Imagen:String,
-    ZonasBusqueda:Array,
+    //Imagen:String,
+    //ZonasBusqueda:Array,
+    Imagen: {
+        type: String,
+        default: 'default.jpg' // Valor predeterminado para el campo Imagen
+    },
     ZonasCBBA:Array,
     Rutas:Object
 },{ versionKey: false });
