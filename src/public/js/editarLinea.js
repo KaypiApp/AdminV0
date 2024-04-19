f1ece8aaeccc654f7b145574a56a464fe379adaa
MishelBravo <141872338+MishelBravo@users.noreply.github.com>
2024-04-19 10:28:35 -0400

    Update editarLinea.js

--- src/public/js/editarLinea.js
+++ src/public/js/editarLinea.js
@@ -221,7 +221,7 @@ var ruta1;
 var ruta2;
 //Carga los datos de listLineas con la diferencia que busca a una similitud de ids para cargar la linea que se requiere modiicar
 window.addEventListener('DOMContentLoaded', (e) =>{
-	fetch('http://localhost:3000/listLineas')
+	fetch('https://kaypi-0aad18445e35.herokuapp.com/listLineas')
 	.then(res => res.json())
 	.then(data =>{
 		if(data.response === 'success'){
